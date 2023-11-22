c0af16fbbabd099cffe28cefc58b6c6b8caf6ee2
anhson <sonanhnguyen003@gmail.com>
2023-11-22 08:40:38 +0700

    🚀 initial commit

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import User from '@/schemas/User'
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { connectMongoDB } from "@/util/connect-db";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {},
+
+            async authorize(credentials) {
+                const { email, password } = credentials;
+
+                try {
+                    await connectMongoDB();
+                    const user = await User.findOne({ email });
+
+                    if (user) {
+                        const passwordsMatch = await bcrypt.compare(password, user.password);
+                        if (passwordsMatch) {
+                            return user;
+                        }
+                    }
+                    return null;
+                } catch (error) {
+                    console.log("Error: ", error);
+                }
+            },
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: "/auth/login",
+        
+    },
+    
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,55 @@
+import { connectMongoDB } from "@/util/connect-db";
+import User from "@/schemas/User";
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { NextResponse } from "next/server";
+
+connectMongoDB();
+
+export async function POST(request) {
+    const requestBody = await request.json();
+    const { email, password, confirm } = requestBody;
+
+    if (password != confirm) {
+        return Response.json(
+            {
+                status: 400,
+                fail: "Confirm password does not match",
+                message: ""
+            }
+        )
+    }
+    if (password.length < 8) {
+        return Response.json({
+            status: 400,
+            fail: "Password must have at least 8 characters",
+            message: ""
+        })
+    }
+
+    const user = await User.findOne({ email });
+    
+    if (user != undefined) {
+        return Response.json({
+            status: 400,
+            fail: "User already exists",
+            message: ""
+        })
+    }
+    //* HASH PASSWORD
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password, salt)
+    
+    const newUser = new User({
+        email: email,
+        password: hashedPassword
+    })
+    newUser.save();
+
+    return Response.json({
+        message: "User created successfully",
+        fail: "",
+        savedUser: newUser
+    })
+
+}
