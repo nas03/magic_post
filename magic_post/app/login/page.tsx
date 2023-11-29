0092648d2a16e982e6dcafabd7cbcd1dbd59d646
Babybluesc <hoangthang16052003@gmail.com>
2023-11-29 08:24:38 +0700

    Update Link Navigation

--- magic_post/app/login/page.tsx
+++ magic_post/app/login/page.tsx
@@ -41,11 +41,11 @@ function page() {
           Magic Post    
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <div className=" w-full p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Your Account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="#" onSubmit={(e) => handleSubmit(e)}>
+              <form className="w-full space-y-4 md:space-y-6" action="#" onSubmit={(e) => handleSubmit(e)}>
                   <div>
                       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Your email</label>
                       <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
@@ -66,7 +66,11 @@ function page() {
                         <label className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
                       </div>
                   </div>
-                  <button type="submit" onClick={() => router.push('/adminPage')} className="w-full hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-[#F79132] text-white hover:text-[#F79132] hover:border-2 hover:border-[#F79132] hover:bg-transparent hover:p-2">Login</button>
+                  <div>
+                    <Link href='/adminPage' className=" hover:w-full hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-[#F79132] text-white hover:text-[#F79132] hover:border-2 hover:border-[#F79132] hover:bg-transparent">
+                        Login
+                    </Link>
+                  </div>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up here</a>
                   </p>
