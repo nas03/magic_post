12e8deaf462beb05e7386df7f2da54fb07ecc8a8
Nguyễn Anh Sơn <sonanhnguyen003@gmail.com>
2023-12-07 18:47:04 +0700

    commit?

--- src/app/pages/dashboard/adminPage/components/index.ts
+++
@@ -1,9 +0,0 @@
-import SideLeftBar from "./SideLeftBar";
-import MainContent from "./MainContent";
-import SideRightBar from "./SideRightBar";
-
-export {
-    SideLeftBar,
-    MainContent,
-    SideRightBar
-}
--- src/lib/prisma/index.ts
+++
@@ -1,5 +0,0 @@
-import UserServices from './UserServices';
-import PostServices from './PostServices';
-import PackageServices from './PackageServices';
-
-export { UserServices, PostServices, PackageServices };
